aa0906a7a14eb0a8e7aeb758aeaeb199a1e1f1e5
Ryders <ryders@gmail.com>
2013-03-08 04:15:21 +1100

    added setup.sh script - see file comments

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Tiny piece of bash to manage a ~/.ssh/authorized_keys file on a server. Running the 
+# script will 1) install a cron (hourly) to run #2 if one doesn't exist already, and 
+# 2) download a list of authorized_keys and install it to the server. 
+# 
+# WARNING - This is vulnerable to a man-in-the-middle attack, so make sure you trust
+# whoever is hosting your list (referenced by $keys below) and it's probably a good
+# idea not to publish 'publicly' a list of server using this setup..
+
+# where should we pull a list of authorized_keys from?
+keys=https://raw.github.com/EmpireOne/ssh_keys/master/authorized_keys
+
+# install the authorized_keys commant (pull, all good? mv)
+cmd="mkdir -pm 700 ~/.ssh > /dev/null 2>&1 ; curl --write-out %{http_code} $keys --output ~/.ssh/authorized_keys_dl 2> /dev/null | grep 200 > /dev/null && mv -f ~/.ssh/authorized_keys_dl ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys > /dev/null 2>&1;"
+
+# make a cron job out of this
+job="0 * * * * $cmd"
+
+# install the cron job if it's not already installed.
+cat <(fgrep -i -v "$cmd" <(crontab -l)) <(echo "$job") | crontab -
+
+# run the command
+eval $cmd
