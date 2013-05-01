51b4aac15aa41c70dd979be75578b93a1077f131
Seb T <sebastien@empireone.com.au>
2013-05-01 10:16:17 +1000

    Added support for various environments.

--- setup.sh
+++ setup.sh
@@ -8,8 +8,15 @@
 # whoever is hosting your list (referenced by $keys below) and it's probably a good
 # idea not to publish 'publicly' a list of server using this setup..
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "You must specify the environment to run on (dev, ci or prd)"
+
 # where should we pull a list of authorized_keys from?
-keys=https://raw.github.com/EmpireOne/ssh_keys/master/authorized_keys
+keys=https://raw.github.com/EmpireOne/ssh_keys/master/authorized_keys-$1
 
 # install the authorized_keys commant (pull, all good? mv)
 cmd="mkdir -pm 700 ~/.ssh > /dev/null 2>&1 ; curl --write-out \%{http_code} $keys --output ~/.ssh/authorized_keys_dl 2> /dev/null | grep 200 > /dev/null && mv -f ~/.ssh/authorized_keys_dl ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys > /dev/null 2>&1;"
